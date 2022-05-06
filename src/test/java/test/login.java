b078a2eb4588ddd30ca1f67d276dec6f9084bcfc
Đức Anh nè <imducanhh@gmail.com>
2022-05-07 03:15:17 +0700

    test

---
+++ src/test/java/test/login.java
@@ -0,0 +1,46 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class login {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drv/chromedriver.exe");
+        driver = new ChromeDriver();
+        // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://demo.guru99.com/v4/");
+    }
+
+    @Test
+    public void TC_01_ValidateCurrentUrl() {
+        String loginPageUrl = driver.getCurrentUrl();
+        Assert.assertEquals(loginPageUrl, "https://demo.guru99.com/v4/");
+    }
+
+    @Test
+    public void TC_02_ValidatePageTitle() {
+        String loginPageTitle = driver.getTitle();
+        Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+    }
+
+    @Test
+    public void TC_03_LoginFormDisplayed() {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
